6095aea07a09df69b07ebccb7ad601a04844f46c
liptonzuma <solomonboateng231@gmail.com>
2021-12-02 12:43:52 +0000

    design

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { motion } from 'framer-motion/dist/framer-motion'
-
+import cart from '../assets/icons/shopping-cart.png'
+import wishlist from '../assets/icons/wishlist.png'
 function Navbar() {
     return (
         <nav 
-        className='bg-gray-900 flex justify-between py-4 px-4 text-white'>
+        className='items-center bg-gray-900 flex justify-between py-4 px-4 text-white'>
             {/* logo */}
             <div className='logo-wrapper'>
                 {/* logo icon */}
                 <a href="/" 
                 className='text-2xl font-black logo'>
-                    tekki 
+                    teck<span className='text-red-500'>i</span>
                 </a>
             </div>
             {/* Search box */}
@@ -29,10 +29,23 @@ function Navbar() {
                 </form>
             </div>
             {/* icons */}
-            <div>
-
+            <div className='flex flex-row justify-around'>
+                {/* shopping cart */}
+                <div className='cart relative'>
+                    <img className='w-9'  src={wishlist} alt="shopping cart"/>
+                    <p className='absolute top-0 left-5 text-center badge rounded-full text-sm px-2 bg-red-500 text-white'>
+                        {10}
+                    </p>
+                </div>
+                {/* wishlist */}
+            <div className='cart relative ml-10'>
+                    <img className='w-10'  src={cart} alt="shopping cart"/>
+                    <p className='absolute top-0 left-5 text-center badge rounded-full text-sm px-2 bg-red-500 text-white'>
+                        {10}
+                    </p>
             </div>
-
+            </div>
+            
         </nav>
     )
 }
