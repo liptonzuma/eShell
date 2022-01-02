76669cbdb2453c5c4b27f263389ff6692b8a2833
liptonzuma <solomonboateng231@gmail.com>
2022-01-02 15:08:16 +0000

    home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Navbar from './Navbar'
+import me from '../assets/me.png'
 
 function Home() {
     return (
         <div>
             <Navbar/>
             {/* Content */}
-            <div className='w-screen h-screen bg-yellow-600'>
-
+            <div className='w-screen h-screen bg-gray-800 main'>
+                {/* Text content */}
+                <h1 className='text-gray-200 big'>
+                    I<span className='text-yellow-600'>'</span>m Solomon
+                </h1>
+            
             </div>
         </div>
     )
