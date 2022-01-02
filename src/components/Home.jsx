5de40e33d79200c441b600b13c8b8db5ba84775e
liptonzuma <solomonboateng231@gmail.com>
2022-01-02 15:17:38 +0000

    home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
             {/* Content */}
             <div className='w-screen h-screen bg-gray-800 main'>
                 {/* Text content */}
-                <h1 className='text-gray-200 big'>
+                <h1 className='text-gray-200 big bg-gray-800'>
                     I<span className='text-yellow-600'>'</span>m Solomon
                 </h1>
             
