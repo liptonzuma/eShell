6c6ed0dcb6ced464e60b31e5e1dd8fa64616418c
liptonzuma <solomonboateng231@gmail.com>
2022-01-02 23:52:11 +0000

    breakpoint

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
     return (
         <nav  className='fixed'>
             {/* toggler */}
-            <motion.div className="icon cursor-pointer p-2 m-4 ml-6 lg:ml-10 icon bg-gray-900 text-yellow-600"
+            <motion.div className="icon visible lg:invisible cursor-pointer p-2 m-4 ml-6 lg:ml-10 icon bg-gray-900 text-yellow-600"
                 onClick={()=>setHide(!hide)}
                 
             >
@@ -16,7 +16,25 @@ function Navbar() {
             {/* nav */}
             <motion.div
             animate={hide?{x:-200}:{x:0} }
-             className='w-20 navbar bg-gray-900 text-center h-screen '>
+             className='w-20 visible lg:invisible navbar bg-gray-900 text-center h-screen '>
+                <a className='cursor-pointer text-2xl  p-2 text-center ' title='home'>
+                    <i className="fa fa-home focus:text-gray-300 hover:text-gray-300 text-yellow-600 p-5"></i>
+                </a>
+
+                <a className='cursor-pointer text-2xl  p-2 text-center ' title='about me'>
+                    <i className="fa fa-user focus:text-gray-300 hover:text-gray-300 text-yellow-600 p-5"></i>
+                </a>
+
+                <a className='cursor-pointer text-2xl  p-2 text-center ' title='my works'>
+                    <i className="fa fa-suitcase focus:text-gray-300 hover:text-gray-300 text-yellow-600 p-5"></i>
+                </a>
+
+                <a className='cursor-pointer text-2xl  p-2 text-center ' title="email me">
+                    <i className="fa fa-envelope focus:text-gray-300 hover:text-gray-300 text-yellow-600 p-5"></i>
+                </a>
+            </motion.div>
+
+            <motion.div  className='w-20 invisibile lg:visible navbar bg-gray-900 text-center h-screen '>
                 <a className='cursor-pointer text-2xl  p-2 text-center ' title='home'>
                     <i className="fa fa-home focus:text-gray-300 hover:text-gray-300 text-yellow-600 p-5"></i>
                 </a>
