6a0691b4321bf31c5a3cc9850621c6ba23b8e539
liptonzuma <solomonboateng231@gmail.com>
2022-01-03 16:48:42 +0000

    main

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { motion } from 'framer-motion/dist/framer-motion'
+import coder from '../assets/img/coder.svg';
 import Navbar from './Navbar'
 
 function Home() {
@@ -8,9 +10,17 @@ function Home() {
             {/* Content */}
             <div className='w-screen h-screen bg-yellow-600 main'>
                 {/* Text content */}
-                <h1 className='text-gray-200 big'>
-                    hello
-                </h1>
+               <motion.div className="textContent">
+                        <motion.h1>
+                            I'm Solomon Boateng
+                        </motion.h1>
+               </motion.div>
+               {/* Image container */}
+               <motion.div>
+                   <img 
+                   className='w-4/6'
+                    src={coder} alt="code monkey" />
+               </motion.div>
             
             </div>
         </div>
