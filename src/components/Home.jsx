fda4c7bdaf267221144d9c9e0317e06ac37a81de
liptonzuma <solomonboateng231@gmail.com>
2022-01-03 16:57:23 +0000

    home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,15 +10,15 @@ function Home() {
             {/* Content */}
             <div className='w-screen h-screen bg-yellow-600 main'>
                 {/* Text content */}
-               <motion.div className="textContent">
-                        <motion.h1>
+               <motion.div className="textContent w-1/2 pl-36">
+                        <motion.h1 className="text-4xl font-black text-gray-200">
                             I'm Solomon Boateng
                         </motion.h1>
                </motion.div>
                {/* Image container */}
-               <motion.div className="ImageContainer">
+               <motion.div className="w-1/2 ImageContainer">
                    <img 
-                   className='w-4/6'
+                   className='w-5/6'
                     src={coder} alt="code monkey" />
                </motion.div>
             
